9fb3d46c088a25e6f28a04d6a9a9f1c46f29ee88
sushantbaskota2 <sushantbaskota2@gmail.com>
2020-02-17 21:06:42 -0600

    Added file upload functionality

--- src/index.js
+++ src/index.js
@@ -6,25 +6,9 @@ const port = process.env.PORT || 3000;
 const userRouter = require('./routers/user');
 const taskRouter = require('./routers/task');
 
-// app.use((req, res, next) => {
-//     if (req.method === 'GET') {
-//         res.send('GET requests are disabled');
-//     }
-//     console.log(req.method, req.path);
-//     next();
-// });
-
-// app.use((req, res, next) => {
-//     if (req) {
-//         res.status(503).send('Site under maintainence');
-//     }
-// });
-
 app.use(express.json());
 app.use(userRouter);
 app.use(taskRouter);
-
-const jwt = require('jsonwebtoken');
 
 app.listen(port, () => {
     console.log('Server is up on port ', port);
