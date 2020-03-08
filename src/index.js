bc618c534bc2c3ccbe23864142632aa2d77ab034
sushantbaskota2 <sushantbaskota2@gmail.com>
2020-03-08 18:37:24 -0500

    cors

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 require('./db/mongoose');
+const cors = require('cors');
 const app = express();
 const port = process.env.PORT;
 
@@ -7,6 +8,7 @@ const userRouter = require('./routers/user');
 const taskRouter = require('./routers/task');
 
 app.use(express.json());
+app.use(cors());
 app.use(userRouter);
 app.use(taskRouter);
 
